e5c45498116836d69ac65859717127008d2f8b44
Foysol Ahmed <foysol.sahi@gmail.com>
2021-02-11 12:49:46 +0600

    Search and alert

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
 
 export class Search extends Component {
+    state = {
+        text: ''
+    }
+
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
+        setAlert: PropTypes.func.isRequired
+    }
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    onSubmit = e => {
+        e.preventDefault();
+        if(this.state.text.trim() === '') {
+            this.props.setAlert("Please Enter username", "light");
+            return;
+        }
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: '' });
+    }
+
     render() {
+        const {showClear, clearUsers} = this.props;
+
         return (
             <div>
-                <form className="form">
-                    <input type="text" placeholder="Search Users ..." />
+                <form onSubmit={this.onSubmit} className="form">
+                    <input type="text" name="text" placeholder="Search Users ..." onChange={this.onChange} />
                     <input type="submit" value="Search" className="btn btn-dark btn-block" />
                 </form>
+                {showClear &&
+                    <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+                }
+
             </div>
         )
     }
